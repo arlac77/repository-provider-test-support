82de428c224f2cf61abb1876d42c3971126d80de
Markus Felten <Markus.Felten@gmx.de>
2020-01-28 13:47:28 +0100

    fix: add repository-provider as peer dep

--- src/module.mjs
+++ src/module.mjs
@@ -1,9 +1,9 @@
-import { generateBranchName } from "repository-provider";
 import { StringContentEntry } from "content-entry";
 export * from "./group-list-test.mjs";
 export * from "./repository-list-test.mjs";
 export * from "./provider-test.mjs";
 export * from "./repository-equality-test.mjs";
+export * from "./pull-request-test.mjs";
 
 export async function assertBranch(t, branch, fixture, url) {
   if (fixture === undefined) {
@@ -111,41 +111,6 @@ export async function assertRepo(t, repository, fixture, url) {
       );
     }
   }
-}
-
-export async function pullRequestLivecycle(t, provider, repoName) {
-  const repository = await provider.repository(repoName);
-
-  const name = await generateBranchName(repository, "pr-test/*");
-
-  const destination = await repository.defaultBranch;
-  const source = await destination.createBranch(name);
-
-  const commit = await source.commit("message text", [
-    new StringContentEntry("README.md", `file content #${name}`)
-  ]);
-
-  const pr = await provider.pullRequestClass.open(source, destination, {
-    title: `test pr from ${name}`,
-    body: "this is the body\n- a\n- b\n- c"
-  });
-
-  t.is(pr.source, source, "pull request source");
-  t.is(pr.destination, destination, "pull request destination");
-  t.true(pr.number !== undefined, "pull request number");
-
-  t.is(pr.title, `test pr from ${name}`, "pull request title");
-  t.is(pr.body, "this is the body\n- a\n- b\n- c", "pull request body");
-  t.is(pr.state, "OPEN", "pull request state");
-  t.is(pr.locked, false, "pull request locked");
-  t.is(pr.merged, false, "pull request merged");
-
-  for await (const p of provider.pullRequestClass.list(repository)) {
-    console.log("LIST", p, p.title, pr.number, p.number, pr.equals(p));
-  }
-
-  //await pr.decline();
-  await source.delete();
 }
 
 export async function assertCommit(t, repository, entryName = "README.md") {
