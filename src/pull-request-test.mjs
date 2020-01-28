d25f972fbe5c65f393734f2ea17c9ed0922e1035
Markus Felten <Markus.Felten@gmx.de>
2020-01-28 13:55:42 +0100

    fix: remove repository-provider dependency

--- src/pull-request-test.mjs
+++ src/pull-request-test.mjs
@@ -1,4 +1,3 @@
-import { generateBranchName } from "repository-provider";
 import { StringContentEntry } from "content-entry";
 
 export async function pullRequestLivecycle(t, provider, repoName) {
@@ -36,4 +35,20 @@ export async function pullRequestLivecycle(t, provider, repoName) {
     await source.delete();
   }
   
-  
+  /**
+ * find a new branch name for a given pattern
+ * '*' will be replaced by a number
+ * 'something/*' will get to something/1 something/2 ...
+ * @param {Repository} repository
+ * @param {string} pattern
+ */
+async function generateBranchName(repository, pattern) {
+  let n = 1;
+
+  for await (const b of repository.branches(pattern)) {
+    n++;
+  }
+
+  const name = pattern.replace(/\*/, n);
+  return name;
+}
