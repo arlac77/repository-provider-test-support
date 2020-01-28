5c4179507c8281da9d66d1713e0ec9b9d41936bb
Markus Felten <Markus.Felten@gmx.de>
2020-01-28 13:59:49 +0100

    fix: typo

--- src/repository-equality-test.mjs
+++ src/repository-equality-test.mjs
@@ -1,10 +1,10 @@
 export async function repositoryEqualityTest(t, p1, rn1, rn2) {
     const r1 = await p1.createRepository(rn1);
-    t.false(r1.equal(undefined));
-    t.true(r1.equal(r1));
+    t.false(r1.equals(undefined));
+    t.true(r1.equals(r1));
 
     const r2 = await p1.createRepository(rn2);
-    t.false(r1.equal(r2));
+    t.false(r1.equals(r2));
 }
 
 repositoryEqualityTest.title = (providedTitle = "repository equality", provider) =>
