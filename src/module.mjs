191cf2acec389100016e2135efba7630bc602619
Markus Felten <Markus.Felten@gmx.de>
2021-11-17 21:00:42 +0100

    feat: provide REPOSITORY_NAME_WITH_BRANCH_GITHUB_HTTP + REPOSITORY_NAME_WITH_BRANCH_GITHUB_GIT

--- src/module.mjs
+++ src/module.mjs
@@ -16,6 +16,8 @@ export const REPOSITORY_NAME = "arlac77/sync-test-repository";
 export const REPOSITORY_NAME_GITHUB_HTTP = `https://github.com/${REPOSITORY_NAME}.git`;
 export const REPOSITORY_NAME_GITHUB_GIT = `git@github.com:${REPOSITORY_NAME}.git`;
 export const REPOSITORY_NAME_WITH_BRANCH = REPOSITORY_NAME + "#preserve-for-test";
+export const REPOSITORY_NAME_WITH_BRANCH_GITHUB_HTTP = REPOSITORY_NAME_GITHUB_HTTP + "#preserve-for-test";
+export const REPOSITORY_NAME_WITH_BRANCH_GITHUB_GIT = REPOSITORY_NAME_GITHUB_GIT + "#preserve-for-test";
 
 
 export async function assertBranch(t, branch, fixture, url) {
