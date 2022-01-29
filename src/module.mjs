fbdb3fc2bbd46a02c27f597011ffde82ce2c0eb2
Markus Felten <Markus.Felten@gmx.de>
2022-01-30 00:59:27 +0100

    feat: add BITBUCKET_REPOSITORY_NAME

--- src/module.mjs
+++ src/module.mjs
@@ -13,6 +13,7 @@ export * from "./entry-list-test.mjs";
 export * from "./messages.mjs";
 export * from "./repositories.mjs";
 
+
 export const REPOSITORY_NAME = "arlac77/sync-test-repository";
 export const REPOSITORY_NAME_GITHUB_HTTP = `https://github.com/${REPOSITORY_NAME}.git`;
 export const REPOSITORY_NAME_GITHUB_GIT = `git@github.com:${REPOSITORY_NAME}.git`;
@@ -20,6 +21,7 @@ export const REPOSITORY_NAME_WITH_BRANCH = REPOSITORY_NAME + "#preserve-for-test
 export const REPOSITORY_NAME_WITH_BRANCH_GITHUB_HTTP = REPOSITORY_NAME_GITHUB_HTTP + "#preserve-for-test";
 export const REPOSITORY_NAME_WITH_BRANCH_GITHUB_GIT = REPOSITORY_NAME_GITHUB_GIT + "#preserve-for-test";
 
+export const BITBUCKET_REPOSITORY_NAME = REPOSITORY_NAME;
 
 export async function assertBranch(t, branch, fixture, url) {
   if (fixture === undefined) {
