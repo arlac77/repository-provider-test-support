35a7f7d3ce4b2099f835396791410cc179ead6a0
Markus Felten <Markus.Felten@gmx.de>
2022-04-22 18:42:29 +0200

    feat: more levels

--- src/messages.mjs
+++ src/messages.mjs
@@ -1,11 +1,11 @@
 export function createMessageDestination() {
-    const messages = {};
-    const messageDestination = {
-      info: (...args) => (messages.info = [...args]),
-      error: (...args) => (messages.error = [...args]),
-      warn: (...args) => (messages.warn = [...args])
-    };
-  
-    return { messageDestination, messages };
-  }
-  
+  const messages = {};
+  const messageDestination = {
+    trace: (...args) => (messages.trace = [...args]),
+    info: (...args) => (messages.info = [...args]),
+    error: (...args) => (messages.error = [...args]),
+    warn: (...args) => (messages.warn = [...args])
+  };
+
+  return { messageDestination, messages, levels: ["trace", "info", "error", "warn"]};
+}
