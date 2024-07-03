49f6902ee86c6975078294040c26ba8e4fd1be2e
Markus Felten <Markus.Felten@gmx.de>
2024-07-03 12:07:03 +0200

    fix: cleanup

--- src/repository-list-test.mjs
+++ src/repository-list-test.mjs
@@ -1,5 +1,4 @@
 export async function repositoryListTest(t, provider, pattern, expected) {
-
   t.truthy(provider, "provider present");
 
   const rs = {};
@@ -8,32 +7,36 @@ export async function repositoryListTest(t, provider, pattern, expected) {
     rs[r.fullName] = r;
   }
 
-  if (typeof expected === "number") {
-    const length = Object.keys(rs).length;
-
-    t.truthy(expected <= length, `expected at least ${expected} but got only ${length} entries for ${pattern}`);
-    return;
-  }
-
-  if (expected === undefined) {
-    t.is(
-      Object.keys(rs).length,
-      0,
-      `there should not by any repository for \"${pattern}\"`
-    );
-  } else {
-    for (const [name, e] of Object.entries(expected)) {
-      const r = rs[name];
+  switch (typeof expected) {
+    case "number": {
+      const length = Object.keys(rs).length;
 
       t.truthy(
-        r !== undefined,
-        `missing expected repository ${name} in (${Object.keys(rs)})`
+        expected <= length,
+        `expected at least ${expected} but got only ${length} entries for ${pattern}`
       );
-
-      for (const key of Object.keys(e)) {
-        t.is(r[key], e[key], `${name}.${key}`);
-      }
+      return;
     }
+    case "undefined":
+      t.is(
+        Object.keys(rs).length,
+        0,
+        `there should not by any repository for \"${pattern}\"`
+      );
+      break;
+    default:
+      for (const [name, e] of Object.entries(expected)) {
+        const r = rs[name];
+
+        t.truthy(
+          r !== undefined,
+          `missing expected repository ${name} in (${Object.keys(rs)})`
+        );
+
+        for (const key of Object.keys(e)) {
+          t.is(r[key], e[key], `${name}.${key}`);
+        }
+      }
   }
 }
 
