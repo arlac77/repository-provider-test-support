7622e165ebcfd92c6169e06fcedf66455365c6a2
Markus Felten <Markus.Felten@gmx.de>
2025-08-14 16:42:55 +0100

    fix: try true false values of isProtected

--- src/branch-test.mjs
+++ src/branch-test.mjs
@@ -1,4 +1,7 @@
 export async function assertBranchUpdateAttributes(t, branch) {
   branch.updateAttributes({ protected: true });
   t.is(branch.isProtected, true);
+
+  branch.updateAttributes({ protected: false });
+  t.is(branch.isProtected, false);
 }
